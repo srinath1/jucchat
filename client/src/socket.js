4e2a5b17bb3b393ae84a98b77a0ec42326bb3390
Srinath N.S <srinath1@users.noreply.github.com>
2022-05-30 22:13:39 +0200

    Update socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = https://jucchat.herokuapp.com/;
+const URL = 'https://jucchat.herokuapp.com/';
 
 const socket = io(URL, {
   path: "/socket.io",
